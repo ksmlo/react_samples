9eb5c4f5290de4c551156f3458bc8271ed7bd06e
ksmlo <m.kasa@leihauoli.com>
2020-04-09 13:03:17 +0900

    pagination

--- src/components/pages/pagination.js
+++ src/components/pages/pagination.js
@@ -1,18 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import { range } from "lodash"
+import ClassNames from "classnames"
 
-const PaginationComponent = () => {
+const PaginationComponent = props => {
   return (
     <ul>
-      <li>
+      <li className="btn-circle" onClick={() => props.change(0)}>
         <i className="material-icons">skip_previous</i>
       </li>
-      <li>
+      <li
+        className="btn-circle"
+        onClick={() => props.change(props.current - 1)}
+      >
         <i className="material-icons">navigate_before</i>
       </li>
-      <li>
+      {range(props.count).map((index, i) => {
+        return (
+          <li
+            key={i}
+            className={ClassNames("btn-circle", {
+              active: props.current === index,
+            })}
+            onClick={() => props.change(index)}
+          >
+            {index + 1}
+          </li>
+        )
+      })}
+      <li
+        className="btn-circle"
+        onClick={() => props.change(props.current + 1)}
+      >
         <i className="material-icons">navigate_next</i>
       </li>
-      <li>
+      <li className="btn-circle" onClick={() => props.change(props.count - 1)}>
         <i className="material-icons">skip_next</i>
       </li>
     </ul>
@@ -20,10 +41,15 @@ const PaginationComponent = () => {
 }
 
 export const Pagination = () => {
+  const [current, changeCurrent] = useState(0)
   return (
     <section className="pagination">
       <h1>ページネーション</h1>
-      <PaginationComponent />
+      <PaginationComponent
+        count={10}
+        current={current}
+        change={p => changeCurrent(p)}
+      />
     </section>
   )
 }

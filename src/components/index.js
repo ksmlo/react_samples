2e52da3d290c3343e168336441dc2ecd8b56e2c0
ksmlo <m.kasa@leihauoli.com>
2020-04-10 20:46:41 +0900

    counter

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,27 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { Router, Route, Switch } from "react-router"
 import { createBrowserHistory } from "history"
+import dayjs from "dayjs"
 
 import { Header } from "./templates/header"
 import { ROUTE } from "./route/index"
 
 export const Main = props => {
-  return (
+  const [now, setNow] = useState(new Date())
+  const openTime = new Date(2020, 3, 11, 20)
+  const isBeforeOpen = dayjs(now).isAfter(openTime)
+  const animationId = useRef()
+  useEffect(() => {
+    if (!isBeforeOpen) {
+      animationId.current = requestAnimationFrame(() => {
+        setNow(new Date())
+      })
+    } else if (dayjs(now).isSame(dayjs(openTime), "m")) {
+      window.location.reload()
+    }
+  })
+  const lastTime = dayjs(openTime).diff(now)
+  return isBeforeOpen ? (
     <Router history={createBrowserHistory()}>
       <div className="main">
         <Header />
@@ -22,5 +37,9 @@ export const Main = props => {
         </Switch>
       </div>
     </Router>
+  ) : (
+    <div className="timer-wrapper">
+      <span>{lastTime}</span>
+    </div>
   )
 }
